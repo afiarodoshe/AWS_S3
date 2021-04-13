301ba3a84e1909281eafc292c6b22d5a84ca51ac
afiarodoshe <45148820+afiarodoshe@users.noreply.github.com>
2021-04-13 14:41:56 +0600

    application to store, delete, download file from amazon AWS S3 bucket

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module main.go
 
 go 1.16
 
-require github.com/aws/aws-sdk-go v1.38.14
+require (
+	github.com/aws/aws-sdk-go v1.38.14
+	github.com/gin-gonic/gin v1.6.3
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,79 +1,178 @@
 package main
 
 import (
-	"context"
-	"flag"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+	"net/http"
 	"os"
-	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-// Uploads a file to S3 given a bucket and object key. Also takes a duration
-// value to terminate the update if it doesn't complete within that time.
-//
-// The AWS Region needs to be provided in the AWS shared config or on the
-// environment variable as `AWS_REGION`. Credentials also must be provided
-// Will default to shared config file, but can load from environment if provided.
-//
-// Usage:
-//   # Upload myfile.txt to myBucket/myKey. Must complete within 10 minutes or will fail
-//   go run withContext.go -b mybucket -k myKey -d 10m < myfile.txt
-func main() {
-	var bucket, key string
-	var timeout time.Duration
+var AccessKeyID string
+var SecretAccessKey string
+var MyRegion string
+var MyBucket string
+var filepath string
 
-	flag.StringVar(&bucket, "b", "", "Bucket name.")
-	flag.StringVar(&key, "k", "", "Object key name.")
-	flag.DurationVar(&timeout, "d", 0, "Upload timeout.")
-	flag.Parse()
+//GetEnvWithKey : get env value
+func GetEnvWithKey(key string) string {
+	return os.Getenv(key)
+}
 
-	// All clients require a Session. The Session provides the client with
-	// shared configuration such as region, endpoint, and credentials. A
-	// Session should be shared where possible to take advantage of
-	// configuration and credential caching. See the session package for
-	// more information.
-	sess := session.Must(session.NewSession())
-
-	// Create a new instance of the service's client with a Session.
-	// Optional aws.Config values can also be provided as variadic arguments
-	// to the New function. This option allows you to provide service
-	// specific configuration.
-	svc := s3.New(sess)
-
-	// Create a context with a timeout that will abort the upload if it takes
-	// more than the passed in timeout.
-	ctx := context.Background()
-	var cancelFn func()
-	if timeout > 0 {
-		ctx, cancelFn = context.WithTimeout(ctx, timeout)
-	}
-	// Ensure the context is canceled to prevent leaking.
-	// See context package for more information, https://golang.org/pkg/context/
-	defer cancelFn()
-
-	// Uploads the object to S3. The Context will interrupt the request if the
-	// timeout expires.
-	_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-		Body:   os.Stdin,
-	})
+func LoadEnv() {
+	err := godotenv.Load(".env")
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
-			// If the SDK can determine the request or retry delay was canceled
-			// by a context the CanceledErrorCode error code will be returned.
-			fmt.Fprintf(os.Stderr, "upload canceled due to timeout, %v\n", err)
-		} else {
-			fmt.Fprintf(os.Stderr, "failed to upload object, %v\n", err)
-		}
+		log.Fatalf("Error loading .env file")
 		os.Exit(1)
 	}
+}
 
-	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, key)
+func ConnectAws() *session.Session {
+	AccessKeyID = GetEnvWithKey("AWS_ACCESS_KEY_ID")
+	SecretAccessKey = GetEnvWithKey("AWS_SECRET_ACCESS_KEY")
+	MyRegion = GetEnvWithKey("AWS_REGION")
+
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String(MyRegion),
+			Credentials: credentials.NewStaticCredentials(
+				AccessKeyID,
+				SecretAccessKey,
+				"", // a token will be created when the session it's used.
+			),
+		})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return sess
+}
+
+func UploadFile(c *gin.Context) {
+	sess := c.MustGet("sess").(*session.Session)
+	uploader := s3manager.NewUploader(sess)
+
+	MyBucket = GetEnvWithKey("BUCKET_NAME")
+
+	file, header, err := c.Request.FormFile("file")
+	filename := header.Filename
+	up, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(MyBucket),
+		ACL:    aws.String("public-read"),
+		Key:    aws.String(filename),
+		Body:   file,
+	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":    "Failed to upload file",
+			"uploader": up,
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"file successfully added ": filename ,
+	})
+}
+
+func UploadFiles(c *gin.Context) {
+
+	MyBucket = GetEnvWithKey("BUCKET_NAME")
+	sess := c.MustGet("sess").(*session.Session)
+	svc:= s3manager.NewUploader(sess)
+	file,header, _ := c.Request.FormFile("file")
+	filename := header.Filename
+	files := []s3manager.BatchUploadObject{
+		{
+			Object:	&s3manager.UploadInput {
+				Bucket: aws.String(MyBucket),
+				Key: aws.String(filename),
+				Body:   file,
+			},
+		},
+	}
+
+	iter := &s3manager.UploadObjectsIterator{Objects: files}
+	if err := svc.UploadWithIterator(aws.BackgroundContext(), iter); err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"file successfully added ": filename ,
+	})
+}
+
+func DeleteFile(c *gin.Context) {
+	svc := s3.New(session.New())
+	MyBucket = GetEnvWithKey("BUCKET_NAME")
+	Key := c.Request.FormValue("Key")
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(MyBucket),
+		Key:    aws.String(Key),
+	}
+	result, err := svc.DeleteObject(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+	c.JSON(http.StatusOK, gin.H{
+		"delete successful ": Key ,
+	})
+	return
+
+}
+
+func DownloadFiles(c *gin.Context)  {
+	sess := session.Must(session.NewSession())
+	downloader := s3manager.NewDownloader(sess)
+	MyBucket = GetEnvWithKey("BUCKET_NAME")
+	Key := c.Request.FormValue("Key")
+	f, err := os.Create(Key)
+	n, err := downloader.Download(f, &s3.GetObjectInput{
+		Bucket: aws.String(MyBucket),
+		Key:    aws.String(Key),
+	})
+	if err != nil {
+		return
+	}
+	fmt.Println(n)
+	c.JSON(http.StatusOK, gin.H{
+		"download successful ": Key ,
+	})
+}
+
+func main() {
+	LoadEnv()
+
+	sess := ConnectAws()
+	router := gin.Default()
+	router.Use(func(c *gin.Context) {
+		c.Set("sess", sess)
+		c.Next()
+	})
+
+	router.POST("/upload", UploadFile)
+	router.POST("/uploads", UploadFiles)
+	router.DELETE("/delete", DeleteFile)
+	router.GET("/download",DownloadFiles)
+
+	_ = router.Run(":3030")
 }
